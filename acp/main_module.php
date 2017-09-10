d82998830726e7d52a2be6296dc060c5972aa334
Steve Guidetti <stevotvr@gmail.com>
2017-09-10 14:36:47 -0700

    Remove extra checks in the main module

--- acp/main_module.php
+++ acp/main_module.php
@@ -54,22 +54,13 @@ class main_module
 			$config->set('stevotvr_steamstatus_refresh_time', max($refresh_time, 0));
 
 			$show_on_profile = $request->variable('steamstatus_show_on_profile', 0);
-			if (strlen($show_on_profile))
-			{
-				$config->set('stevotvr_steamstatus_show_on_profile', $show_on_profile ? 1 : 0);
-			}
+			$config->set('stevotvr_steamstatus_show_on_profile', $show_on_profile ? 1 : 0);
 
 			$show_on_viewtopic = $request->variable('steamstatus_show_on_viewtopic', 0);
-			if (strlen($show_on_viewtopic))
-			{
-				$config->set('stevotvr_steamstatus_show_on_viewtopic', $show_on_viewtopic ? 1 : 0);
-			}
+			$config->set('stevotvr_steamstatus_show_on_viewtopic', $show_on_viewtopic ? 1 : 0);
 
 			$reg_field = $request->variable('steamstatus_reg_field', 0);
-			if (strlen($reg_field))
-			{
-				$config->set('stevotvr_steamstatus_reg_field', $reg_field ? 1 : 0);
-			}
+			$config->set('stevotvr_steamstatus_reg_field', $reg_field ? 1 : 0);
 
 			if (!count($error))
 			{
