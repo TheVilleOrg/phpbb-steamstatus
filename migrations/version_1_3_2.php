856ea5386ec0c003c7d50cf3281a5d0f4833c7fd
Steve Guidetti <stevotvr@gmail.com>
2021-01-17 18:36:23 -0800

    Clear the profile cache when upgrading

---
+++ migrations/version_1_3_2.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * Steam Status. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2021, Steve Guidetti, https://github.com/stevotvr
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace stevotvr\steamstatus\migrations;
+
+use phpbb\db\migration\migration;
+
+/**
+ * Steam Status migration for version 1.3.2.
+ */
+class version_1_3_2 extends migration
+{
+	static public function depends_on()
+	{
+		return array('\stevotvr\steamstatus\migrations\version_1_3_0');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('custom', array(array($this, 'purge_cache'))),
+		);
+	}
+
+	/**
+	 * Purge all cached Steam profiles.
+	 */
+	public function purge_cache() {
+		$this->db->sql_query('DELETE FROM ' . $this->table_prefix . 'steamstatus');
+	}
+}
